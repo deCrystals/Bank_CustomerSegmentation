a1f8e09bdc58f62b55804394b139a8b6fc737ac9
Opeyemi Abodunrin <120110318+deCrystals@users.noreply.github.com>
2025-05-08 22:50:19 +0100

    correct

--- Notebook/main.py
+++ Notebook/main.py
@@ -9,7 +9,7 @@ from segmentation import segment_customer
 
 print("Loading Data..........")
 
-df = preprocess('../Data/bank_data_C.csv')
+df = preprocess('/Data/bank_data_C.csv')
 
 print("Data loaded successfully")
 
@@ -41,31 +41,7 @@ print(f' The Cluster Scores : \n {cluster_scores}')
 
 print("Clustering Completed Sucessfully")
 
-rfm.to_csv('../Data/segmented_customer.csv')
+rfm.to_csv('/Data/segmented_customer.csv')
 
 print("Segmented customer data saved to 'Data/segmented_customers.csv")
 
-
-'''
-rfm_seg =customer_segments(rfm)
-
-rfm_features = rfm[['Recency', 'Frequency', 'Monetary']].copy()
-
-print("RFM Completed")   
-
-print("Starting Clustering........")
-
-scaler = StandardScaler()
-rfm_scaled = scaler.fit_transform(rfm_features)
-n_clusters = 4
-
-kmeans = KMeans(n_clusters=n_clusters, random_state=42)
-rfm['Cluster'] = kmeans.fit_predict(rfm_scaled)
-
-print('Clustering Completed')
-
-cluster_centers = scaler.inverse_transform(kmeans.cluster_centers_)
-print(f'Cluster _centers =  {cluster_centers}')
-
-cluster_scores = rfm.groupby('Cluster')[['Recency', 'Frequency', 'Monetary']].mean().round(1)
-print(f' The Cluster Scores : \n {cluster_scores}') '''
